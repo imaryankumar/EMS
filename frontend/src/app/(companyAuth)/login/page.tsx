f746c8443935533bf77cdcd75d010afd09f735dd
Aryan Kumar <aryan8910kumar@gmail.com>
2025-01-15 01:14:32 +0530

    add:company signup page

---
+++ frontend/src/app/(companyAuth)/login/page.tsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="w-full h-screen">
+      <div className="w-full h-full flex items-center justify-between">
+        <div className="w-4/6 h-full bg-[url(/images/blue-toned-set-paper-sheets-with-copy-space.jpg)] bg-cover bg-center bg-no-repeat">
+          <h1></h1>
+        </div>
+        <div className="w-2/6 h-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
